7504427303dfc74039440080cd58304f6649d4c4
Sara Oliver <47860987+SOliv1@users.noreply.github.com>
2022-07-24 17:20:12 +0100

    Update App.js
    Change name from Codeacademy to Sara! to watch for changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello Codecademy!
+         Hello Sara!
         </p>
         <a
           className="App-link"
